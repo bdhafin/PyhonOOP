64c031cb16d15240f58867b45d6063f1bf7d21cd
dhafinr <dhafinriz050500@gmail.com>
2019-11-24 12:36:42 +0700

    complete oop

---
+++ #1Pendahuluan Object Oriented Programming/main.py
@@ -0,0 +1,17 @@
+#template
+class Hero:
+    pass
+
+
+hero1 = Hero() #object / instances (instansiate)
+hero2 = Hero()
+
+#memberi attribut pada object
+hero1.name = 'sniper'
+hero1.health = 100
+
+hero2.name = 'sven'
+hero2.health = 200
+
+#cara akses attribut
+print(hero1.name)
