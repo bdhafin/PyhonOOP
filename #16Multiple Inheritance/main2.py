5aa9021d3a52a4047a48dd760efb6bfa48cd1856
dhafinr <dhafinriz050500@gmail.com>
2019-11-25 13:44:44 +0700

    complete exam

--- #16Multiple Inheritance/main2.py
+++ #16Multiple Inheritance/main2.py
@@ -1,2 +1,26 @@
-class Hero:
+class Team:
+    def set_team(self, team):
+        self.team = team
+
+    def show_team(self):
+        print(self.team)
+
+class Tipe_Hero:
+    def set_tipe(self, tipe):
+        self.tipe = tipe
+
+    def show_tipe(self):
+        print(self.tipe)
+
+class Hero(Team, Tipe_Hero):
     
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+sniper = Hero('sniper', 100)
+sniper.set_team('dire')
+sniper.set_tipe('carry')
+
+sniper.show_team()
+sniper.show_tipe()
